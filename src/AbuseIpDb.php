0b3affe28f8b31c00c4ffec1686daed2e4f52185
Joby Elliott <code@joby.lol>
2026-03-24 09:24:12 -0600

    added missing comment

--- src/AbuseIpDb.php
+++ src/AbuseIpDb.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * smolSentry
+ * https://github.com/joby-lol/smol-sentry
+ * (c) 2024-2025 Joby Elliott code@joby.lol
+ * MIT License https://opensource.org/licenses/MIT
+ */
+
 namespace Joby\Smol\Sentry;
 
 use InvalidArgumentException;
